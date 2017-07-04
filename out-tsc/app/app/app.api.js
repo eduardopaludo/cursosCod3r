65a958d8ab54a1db337847325de641fad47a7b37
Eduardo Henrique Paludo <eduardo.paludo@sicoobsc.com.br>
2017-07-04 08:31:36 -0300

    Primeiro commit

---
+++ out-tsc/app/app/app.api.js
@@ -0,0 +1,2 @@
+export var MEAT_API = 'http://localhost:300';
+//# sourceMappingURL=app.api.js.map
---
+++ out-tsc/app/app/app.routes.js
@@ -0,0 +1,9 @@
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+export var ROUTES = [
+    { path: '', component: HomeComponent },
+    { path: 'restaurants', component: RestaurantsComponent },
+    { path: 'about', component: AboutComponent }
+];
+//# sourceMappingURL=app.routes.js.map
